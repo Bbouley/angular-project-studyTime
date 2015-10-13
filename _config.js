5d7e5c4aaf90a7aff0e10cc0bf281f1c67de703a
bbouley <b.bouley@hotmail.co.uk>
2015-10-13 16:22:04 -0600

    edited mongo database

--- _config.js
+++ _config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.mongoURI = {
-  development: 'mongodb://localhost/study-time',
+  development: 'mongodb://heroku_3dnnr5lq:feohu0mm6unv6vq093boue8kn6@ds037234.mongolab.com:37234/heroku_3dnnr5lq',
   test: 'mongodb://localhost/study-time-testing'
 };
 
